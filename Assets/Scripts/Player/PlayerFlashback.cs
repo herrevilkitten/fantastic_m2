f69adf14435b27ebfb629df59dacc2d4e4ed5459
redcoatmedia <gina@redcoatmedia.com>
2015-06-17 00:20:03 -0400

    Added brushes, added the ragdoll effect when you hit the car flashback. remember the car accident.

---
+++ Assets/Scripts/Player/PlayerFlashback.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFlashback : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Debug.Log (hit.collider.tag);
+		Debug.Log ("I've collided with something");
+		if (hit.collider.tag == "Flashback") {
+			SetKinematic (false);
+			GetComponent<Animator> ().enabled = false;
+		}
+	}
+
+	void SetKinematic(bool newValue)
+	{
+		Rigidbody[] bodies=GetComponentsInChildren<Rigidbody>();
+		
+		foreach (Rigidbody rigidParts in bodies) {
+			rigidParts.isKinematic=newValue;
+		}
+	}
+}
