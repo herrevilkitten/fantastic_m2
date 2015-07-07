e0f576b80a4a4d608997eef915c34495348f8ef6
redcoatmedia <gina@redcoatmedia.com>
2015-07-07 11:41:14 -0400

    added logic for timing

--- Assets/AI/Actions/DetectPlayerPosition.cs
+++ Assets/AI/Actions/DetectPlayerPosition.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RAIN.Action;
 using RAIN.Core;
+using System;
 
 [RAINAction]
 public class DetectPlayerPosition : RAINAction
@@ -23,16 +24,31 @@ public class DetectPlayerPosition : RAINAction
 		currentPosition.z = player.transform.position.z;
 		
 		Vector3 lastPosition = ai.WorkingMemory.GetItem<Vector3>("playerNewPosition");
+		float lastPositionTime = ai.WorkingMemory.GetItem<float>("playerNewPositionTime");
 		
 		ai.WorkingMemory.SetItem ("playerLastPosition", lastPosition);
 		ai.WorkingMemory.SetItem ("playerNewPosition", currentPosition);
+
+		ai.WorkingMemory.SetItem ("playerLastPositionTime", lastPositionTime);
+		ai.WorkingMemory.SetItem ("playerNewPositionTime", GetCurrentMillis());
+
 		
 		Debug.Log ("my character position = " + ai.Body.transform.position);
+		Debug.Log ("GetCurrentMillis = " + GetCurrentMillis());
+		Debug.Log ("lastPositionTime = " + lastPositionTime);
+		Debug.Log ("difference =" + (GetCurrentMillis () - lastPositionTime));
 		
 		ai.WorkingMemory.SetItem ("headoffPosition", currentPosition);
 
         return ActionResult.SUCCESS;
     }
+
+	private static float GetCurrentMillis()
+	{
+		DateTime Jan1970 = new DateTime(1970, 1, 1, 0, 0,0,DateTimeKind.Utc);
+		TimeSpan javaSpan = DateTime.UtcNow - Jan1970;
+		return (float) javaSpan.TotalMilliseconds;
+	}
 
     public override void Stop(RAIN.Core.AI ai)
     {
