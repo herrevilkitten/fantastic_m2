b6edc588c6761b0a8ad238f89c6da79e82c3b836
redcoatmedia <gina@redcoatmedia.com>
2015-07-08 21:39:18 -0400

    made the aj secret stop and start appropriately

--- Assets/AI/Actions/IsStationary.cs
+++ Assets/AI/Actions/IsStationary.cs
@@ -7,61 +7,80 @@ using RAIN.Core;
 [RAINAction]
 public class IsStationary : RAINAction
 {
-    public override void Start(RAIN.Core.AI ai)
-    {
-        base.Start(ai);
-    }
-
-    public override ActionResult Execute(RAIN.Core.AI ai)
-    {
+	private const int TRIGGER_START_ANIMATION = 0;
+	private const int ALLOW_ANIMATION_TO_FINISH = 1;
+	private const int TRIGGER_STOP_ANIMATION = 2;
+	
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+	}
+	
+	public override ActionResult Execute(RAIN.Core.AI ai)
+	{
 		Debug.Log ("Entering IsStationary Logic");
 		Vector3 lastPosition = ai.WorkingMemory.GetItem <Vector3> ("npcLastPosition");
-
+		
 		if (!ObjectInteractionUtilities.IsPlayerMoving (ai)) {
 			Debug.Log ("Player is not moving");
-			if (ObjectInteractionUtilities.IsPlayerCloseToNPC (ai, 1.0)) {
+			if (ObjectInteractionUtilities.IsPlayerCloseToNPC (ai, 1.5)) {
 				Debug.Log ("Player is close to NPC");
 				float timeSinceStartOfGame = Time.time;
-
-				if (IsTimeToTriggerAnimation(ai, timeSinceStartOfGame, 30.0f)) {
+				
+				int isTriggerAnim = IsTimeToTriggerAnimation(ai, timeSinceStartOfGame, 30.0f);
+				
+				
+				if (isTriggerAnim==TRIGGER_START_ANIMATION) {
+					Debug.Log ("Trigger animation");
 					ai.WorkingMemory.SetItem ("trigSecrAnim", true);	
 					ai.WorkingMemory.SetItem ("lastSecretTriggerTime", timeSinceStartOfGame);
-				} else {
-					Debug.Log ("Not triggering animation b/c it was done within the last 10 seconds");
+				} 
+				
+				if (isTriggerAnim==ALLOW_ANIMATION_TO_FINISH) {
+					Debug.Log ("Continue animation");
+					ai.WorkingMemory.SetItem ("trigSecrAnim", true);	
+				} 
+				
+				if (isTriggerAnim==TRIGGER_STOP_ANIMATION) {
+					Debug.Log ("Player is NOT close to NPC");
 					ai.WorkingMemory.SetItem ("trigSecrAnim", false);
 				}
-			} else {
-				Debug.Log ("Player is NOT close to NPC");
-				ai.WorkingMemory.SetItem ("trigSecrAnim", false);
 			}
 		} else {
 			Debug.Log ("Player is moving");
 			ai.WorkingMemory.SetItem ("trigSecrAnim", false);
 		}
-
+		
 		ai.WorkingMemory.SetItem ("npcLastPosition", ai.Body.transform.position);
-
+		
 		Debug.Log ("Exiting IsStationary Logic");
-        return ActionResult.SUCCESS;
-    }
-
-	private bool IsTimeToTriggerAnimation(RAIN.Core.AI ai, float timeSinceStartOfGame, float range) 
+		return ActionResult.SUCCESS;
+	}
+	
+	private int IsTimeToTriggerAnimation(RAIN.Core.AI ai, float timeSinceStartOfGame, float range) 
 	{
 		float lastSecretTriggerTime = ai.WorkingMemory.GetItem<float> ("lastSecretTriggerTime");
-
+		
 		Debug.Log ("timeSinceStartOfGame - lastSecretTriggerTime = " + (timeSinceStartOfGame - lastSecretTriggerTime));
 		if (lastSecretTriggerTime == 0) {
-			return true;
-		} if (timeSinceStartOfGame - lastSecretTriggerTime > range) {
-			return true;
-		} else {
-			return false;
-		}
+			return TRIGGER_START_ANIMATION;
+		} 
+		
+		if (timeSinceStartOfGame - lastSecretTriggerTime < 15.0f) {
+			//let the clip finish playing before deactivating?
+			return ALLOW_ANIMATION_TO_FINISH;
+		} 
+		
+		if (timeSinceStartOfGame - lastSecretTriggerTime > range) {
+			return TRIGGER_START_ANIMATION;
+		} 
+		
+		return TRIGGER_STOP_ANIMATION;
 	}
-
-    public override void Stop(RAIN.Core.AI ai)
-    {
-        base.Stop(ai);
-    }
-
+	
+	public override void Stop(RAIN.Core.AI ai)
+	{
+		base.Stop(ai);
+	}
+	
 }
