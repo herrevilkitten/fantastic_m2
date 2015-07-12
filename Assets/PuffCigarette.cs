787b3c08a589076084be244c164b59c70affd1b4
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-07-11 20:25:15 -0400

    Lots of dialog and stuff changes

---
+++ Assets/PuffCigarette.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuffCigarette : MonoBehaviour
+{
+	public ParticleSystem particleSystem;
+
+	public void Puff (string stuff)
+	{
+		Debug.Log ("Puffing on a cigarette");
+		if (particleSystem == null) {
+			return;
+		}
+
+		particleSystem.Play ();
+	}
+}
