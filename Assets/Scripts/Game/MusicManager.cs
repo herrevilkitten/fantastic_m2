2b1b2e8d21b4c3678af3eabe7cea201ba5a3fb0c
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-07-13 22:12:11 -0400

    Title sequence changes

---
+++ Assets/Scripts/Game/MusicManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+	public AudioClip titleClip;
+	public AudioClip gameClip;
+	public AudioClip creditsClip;
+
+	AudioSource audioSource;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void PlayTitleClip ()
+	{
+		audioSource.clip = titleClip;
+		audioSource.Play ();
+	}
+
+	public void PlayGameClip ()
+	{
+		audioSource.clip = gameClip;
+		audioSource.Play ();
+	}
+	
+	public void PlayCreditsClip ()
+	{
+		audioSource.clip = creditsClip;
+		audioSource.Play ();
+	}
+	
+	public void SetMusicVolume (float volume)
+	{
+		audioSource.volume = volume;
+	}
+
+	public float GetMusicVolume ()
+	{
+		return audioSource.volume;
+	}
+
+	public void ToggleMusic ()
+	{
+		audioSource.mute = !audioSource.mute;
+	}
+}
---
+++ Assets/TitleScripting.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScripting : MonoBehaviour
+{
+	enum TitleState
+	{
+		SHOW_PRESENTS,
+		HIDE_PRESENTS,
+		SHOW_PRODUCTION,
+		HIDE_PRODUCTION,
+		SHOW_LOGO
+	}
+	;
+
+	TitleState titleState = TitleState.SHOW_PRESENTS;
+
+	public Image logo;
+	public Text presents;
+	public Text production;
+	public MusicManager musicManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StateManager.Pause ();
+		musicManager.PlayTitleClip ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log ("Title state: " + titleState);
+		switch (titleState) {
+		case TitleState.SHOW_PRESENTS:
+			presents.color = new Color (presents.color.r, presents.color.g, presents.color.b, Mathf.Lerp (presents.color.a, 1.0f, .05f));
+			Debug.Log ("Presents Color: " + presents.color);
+			if (presents.color.a >= .9f) {
+				titleState = TitleState.HIDE_PRESENTS;
+			}
+			break;
+		case TitleState.HIDE_PRESENTS:
+			presents.color = new Color (presents.color.r, presents.color.g, presents.color.b, Mathf.Lerp (presents.color.a, 0f, .05f));
+			Debug.Log ("Presents Color: " + presents.color);
+			if (presents.color.a <= .01f) {
+				presents.color = new Color (presents.color.r, presents.color.g, presents.color.b, 0f);
+				titleState = TitleState.SHOW_PRODUCTION;
+			}
+			break;
+		case TitleState.SHOW_PRODUCTION:
+			production.color = new Color (production.color.r, production.color.g, production.color.b, Mathf.Lerp (production.color.a, 1.0f, .05f));
+			Debug.Log ("Presents Color: " + production.color);
+			if (production.color.a >= .9f) {
+				titleState = TitleState.HIDE_PRODUCTION;
+			}
+			break;
+		case TitleState.HIDE_PRODUCTION:
+			production.color = new Color (production.color.r, production.color.g, production.color.b, Mathf.Lerp (production.color.a, 0f, .05f));
+			Debug.Log ("Presents Color: " + production.color);
+			if (production.color.a <= .01f) {
+				production.color = new Color (production.color.r, production.color.g, production.color.b, 0f);
+				titleState = TitleState.SHOW_LOGO;
+			}
+			break;
+		case TitleState.SHOW_LOGO:
+			logo.color = new Color (logo.color.r, logo.color.g, logo.color.b, Mathf.Lerp (logo.color.a, 1f, .05f));
+			Debug.Log ("Presents Color: " + logo.color);
+			break;
+		}
+	}
+}
