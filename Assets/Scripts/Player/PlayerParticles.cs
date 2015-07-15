dc3ed358040af6a7b38394aecb869adab8ce0ca8
Matthew Moldavan <m.moldavan@gmail.com>
2015-07-15 00:04:27 -0400

    add player particle script

---
+++ Assets/Scripts/Player/PlayerParticles.cs
@@ -0,0 +1,34 @@
+﻿	using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerParticles : MonoBehaviour
+{
+	public ParticleSystem[] splashParticles;
+
+	void Start ()
+	{
+	}
+	
+	void OnStep (string foot)
+	{
+		string surfaceTag = "";
+		
+		RaycastHit hit;
+		
+		if (Physics.Raycast (transform.position, Vector3.down, out hit)) {
+			surfaceTag = hit.collider.tag;
+		}
+
+		if (surfaceTag.Equals ("Water")) {
+			if(foot.Equals ("leftFoot")) {
+				splashParticles[0].Play ();
+			}
+			else {
+				splashParticles[1].Play ();
+			}
+
+		}
+
+	}
+}
