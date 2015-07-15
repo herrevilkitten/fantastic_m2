00c9ef8db3f63269c854e34dbb0691016467a6a6
redcoatmedia <gina@redcoatmedia.com>
2015-07-15 09:56:41 -0400

    Added sporty granny NPC that walks around

---
+++ Assets/Scripts/Player/PlayerThrowBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerThrowBall : MonoBehaviour {
+
+	bool isWaving = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("t")) {
+			isWaving = !isWaving;
+			Animator animator = GetComponent<Animator>();
+			animator.SetBool("IsWaving", isWaving);
+			/*
+			if (isWaving) {
+				animator.SetLayerWeight(1, 1f);	
+			} else {
+				animator.SetLayerWeight(1, 0f);	
+			}
+*/
+
+		}
+
+	}
+}
