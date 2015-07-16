35a6f507c4650659bf72fb4c812afdbd722ef617
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-07-16 00:31:08 -0400

    IntroductionDialog

---
+++ Assets/IntroductionDialog.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroductionDialog : TalkableObjectWithDialog
+{
+	public override string GetDialogResourceName ()
+	{
+		return "Introduction";
+	}
+
+	bool dialogShown = false;
+
+	void Update ()
+	{
+		if (StateManager.IsPlaying () && !dialogShown) {
+			dialogShown = true;
+			this.OnInteractClick (null);
+		}
+	}
+}
