54e23490a9b17caa7e5d64da756d5dd25524bf48
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-07-23 10:51:12 -0400

    Settings update

---
+++ Assets/QualityChange.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QualityChange : MonoBehaviour
+{
+	public Text label;
+	Slider slider;
+	string[] qualityNames;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		slider = GetComponent<Slider> ();
+
+		qualityNames = QualitySettings.names;
+		slider.minValue = 0;
+		slider.maxValue = qualityNames.Length - 1;
+		slider.value = Mathf.Clamp (PlayerPrefs.GetInt ("Quality"), slider.minValue, slider.maxValue);
+		label.text = qualityNames [(int)slider.value];
+	}
+	
+	void Update ()
+	{
+		slider.onValueChanged.AddListener ((newValue) => {
+			int value = (int)newValue;
+			QualitySettings.SetQualityLevel (value);
+			label.text = qualityNames [value];
+		});
+	}
+}
