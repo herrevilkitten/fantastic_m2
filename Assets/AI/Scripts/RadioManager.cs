ea72338b9a7700a22831d5c97c8de3c47873b659
redcoatmedia <gina@redcoatmedia.com>
2015-07-22 22:27:12 -0400

    Create classes to send messages back and forth between cops

---
+++ Assets/AI/Scripts/RadioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RadioManager {
+	private static RadioManager singleton = new RadioManager();
+	private ArrayList cops = new ArrayList ();
+
+	private RadioManager() {
+	}
+
+	public static RadioManager Singleton {
+		get 
+		{
+			if(singleton == null) {
+				singleton = RadioManager();
+			}
+
+			return singleton;
+		}
+	}
+
+	public void Listen(RadioElement copRadio) {
+		cops.Add (copRadio);
+	}
+
+	public void RadioMessage(string sender, string messageName, object value) {
+		for(int i=0; i<cops.Count; i++) {
+			RadioElement element = cops[i];
+			element.ReceiveMessage(sender, messageName, value);
+		}
+	}
+}
