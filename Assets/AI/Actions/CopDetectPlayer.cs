5177ecd3c8da53c5c4aae77be0ce26b3b7cc4c62
redcoatmedia <gina@redcoatmedia.com>
2015-07-24 20:21:13 -0400

    Last cop tree changes

--- Assets/AI/Actions/CopDetectPlayer.cs
+++ Assets/AI/Actions/CopDetectPlayer.cs
@@ -11,25 +11,35 @@ using System.Collections.Generic;
 public class CopDetectPlayer : RAINAction
 {
 	public override void Start(AI ai){
-		IList<RAINAspect> matches = ai.Senses.Sense("VisualSensor", "PlayerAspect", RAINSensor.MatchType.ALL);
 
-		//Debug.Log ("Looking for matches=" + matches.Count);
-		foreach (RAINAspect aspect in matches)
-		{
-			if (aspect == null)
-				continue;
+		float currentTime = Time.fixedTime;
 
-			if (aspect != null) {
+		if (ShouldLookForPlayer(ai, currentTime)) {
+			IList<RAINAspect> matches = ai.Senses.Sense("VisualSensor", "PlayerAspect", RAINSensor.MatchType.ALL);
+			ai.WorkingMemory.SetItem<float>("LastDetectCycle", currentTime);
 
-				Debug.Log ("InteractionManager = " + InteractionManager.IsPlayerInteractingWithObject());
-				if (InteractionManager.IsPlayerInteractingWithObject()) {
-					RadioManager.Singleton.RadioDispatcher(ai, aspect.MountPoint, Time.deltaTime);
-					break;
-				} else {
-					ai.WorkingMemory.SetItem("currentAction", "patrol");
-					break;
+			foreach (RAINAspect aspect in matches)
+			{
+				if (aspect != null) {
+					
+					Debug.Log ("InteractionManager = " + InteractionManager.IsPlayerInteractingWithObject());
+					if (InteractionManager.IsPlayerInteractingWithObject()) {
+						RadioManager.Singleton.RadioDispatcher(ai, aspect.MountPoint, Time.fixedTime);
+						break;
+					} else {
+						ai.WorkingMemory.SetItem("currentAction", "patrol");
+						break;
+					}
 				}
 			}
 		}
 	}
+
+	public bool ShouldLookForPlayer(AI ai, float currentTime) {
+
+		float lastDetectCycle = ai.WorkingMemory.GetItem<float> ("LastDetectCycle");
+
+		return (lastDetectCycle == 0.0f) || ((lastDetectCycle != 0) && (currentTime - lastDetectCycle) >= 1.5f);
+	}
+
 }
