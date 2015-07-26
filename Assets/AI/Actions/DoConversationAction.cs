5c751ac320542d45d61ba363f3b86964f77e0afc
redcoatmedia <gina@redcoatmedia.com>
2015-07-25 21:21:49 -0400

    Fixed the overhead text to be sharper

--- Assets/AI/Actions/DoConversationAction.cs
+++ Assets/AI/Actions/DoConversationAction.cs
@@ -20,20 +20,13 @@ public class DoConversationAction : RAINAction
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 		if (!commManager.IsFinishedTalking ()) {
-			GameObject currentTalker1 = commManager.GetCurrentClueTalker ();
+			GameObject currentTalker = commManager.GetCurrentClueTalker ();
 
-			string message1 = commManager.NextClueDialog ();
+			string message = commManager.NextClueDialog ();
 
-			GameObject currentTalker2 = commManager.GetCurrentClueTalker ();
-			string message2 = commManager.NextClueDialog ();
+			//DialogManager.Conversation (currentTalker1.name + ": " + message1, displaySecondMessage);
 
-			string displaySecondMessage = "";
-
-			if (message2.Length != 0) {
-				displaySecondMessage = currentTalker2.name + ": " + message2;
-			}
-
-			DialogManager.Conversation (currentTalker1.name + ": " + message1, displaySecondMessage);
+			DialogManager.Floating(currentTalker, message, 5);
 		
 		} else {
 			DialogManager.StopConversation ();
--- Assets/AI/Actions/DoFakeConversationAction.cs
+++ Assets/AI/Actions/DoFakeConversationAction.cs
@@ -21,23 +21,11 @@ public class DoFakeConversationAction : RAINAction
 	{
 		DialogManager.StopConversation();
 		
-		GameObject currentTalker1 = commManager.GetCurrentFakeTalker ();
-		string message1 = commManager.NextFakeDialog ();
+		GameObject currentTalker = commManager.GetCurrentFakeTalker ();
+		string message = commManager.NextFakeDialog ();
 
-		GameObject currentTalker2 = commManager.GetCurrentFakeTalker ();
-		string message2 = commManager.NextFakeDialog ();
-		
-		string displaySecondMessage = "";
-		
-		if (message2.Length != 0) {
-			displaySecondMessage = currentTalker2.name + ": " + message2;
-		}
-		
-		if (message1.Length > 0) {
-			DialogManager.Conversation (currentTalker1.name + ": " + message1, displaySecondMessage);
-		} else {
-			DialogManager.StopConversation();
-		}
+		DialogManager.Floating(currentTalker, message, 5);
+
 		return ActionResult.SUCCESS;
 	}
 	
