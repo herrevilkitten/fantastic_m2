d1dc854f5ddbfd1abd725de5d5d0da035c581f06
Matthew Moldavan <m.moldavan@gmail.com>
2015-07-27 23:39:36 -0400

    add fire

---
+++ Assets/SimpleRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotate : MonoBehaviour {
+	public float rotationAmount = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, rotationAmount * Time.deltaTime, 0);
+	}
+}
