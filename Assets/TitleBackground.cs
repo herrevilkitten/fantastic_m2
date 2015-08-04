50accbe1be3f1bdea5785c09190fee842f04cdaf
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-08-03 22:32:41 -0400

    Title sequence updates

---
+++ Assets/TitleBackground.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleBackground : MonoBehaviour
+{
+	public float fadeTime = 10.0f;
+
+	public Image backgroundImage;
+	public RawImage backgroundMovieImage;
+	public GameObject titlePanel;
+	MovieTexture backgroundMovie;
+	float movieStartTime;
+	float movieEndTime;
+
+	void Awake ()
+	{
+		backgroundMovie = (MovieTexture)backgroundMovieImage.mainTexture;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		movieStartTime = Time.time;
+		movieEndTime = movieStartTime + backgroundMovie.duration - fadeTime;
+		backgroundMovie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (backgroundMovie.isPlaying) {
+			float alpha = Time.deltaTime / (fadeTime / 2f);
+
+			if (Input.GetButtonDown ("Cancel") || Time.time >= (movieEndTime + fadeTime)) {
+				backgroundMovie.Stop ();
+				backgroundMovieImage.enabled = false;
+				titlePanel.SetActive (true);
+			} else if (Time.time >= (movieEndTime + (fadeTime / 2f))) {
+				backgroundImage.color = new Color (backgroundImage.color.r, 
+				                                  backgroundImage.color.g, 
+				                                  backgroundImage.color.b, 
+				                                  backgroundImage.color.a + alpha);
+
+			} else if (Time.time >= movieEndTime) {
+				backgroundMovieImage.color = new Color (backgroundMovieImage.color.r, 
+				                                       backgroundMovieImage.color.g, 
+				                                       backgroundMovieImage.color.b, 
+				                                       backgroundMovieImage.color.a - alpha);
+			}
+		}
+	}
+}
--- Assets/TitleScripting.cs
+++ Assets/TitleScripting.cs
@@ -5,45 +5,13 @@ using System.Collections;
 
 public class TitleScripting : MonoBehaviour
 {
-	enum TitleState
-	{
-		SHOW_PRESENTS,
-		HIDE_PRESENTS,
-		SHOW_PRODUCTION,
-		HIDE_PRODUCTION,
-		SHOW_LOGO,
-		DONE
-	}
-	;
-
-	TitleState titleState = TitleState.SHOW_PRESENTS;
-
-	public Image logo;
-	public Text presents;
-	public Text production;
 	public MusicManager musicManager;
 	public SfxManager sfxManager;
-	public GameObject buttonPanel;
-	public Button startButton;
-	public EventSystem eventSystem;
-
-	RawImage backgroundMovieImage;
-	MovieTexture backgroundMovie;
-	float movieStartTime;
-
-	void Awake ()
-	{
-		backgroundMovieImage = GetComponent<RawImage> ();
-		backgroundMovie = (MovieTexture)backgroundMovieImage.mainTexture;
-	}
 
 	// Use this for initialization
 	void Start ()
 	{
 		InitializePrefs ();
-
-		backgroundMovie.Play ();
-		movieStartTime = Time.time;
 	}
 
 	void InitializePrefs ()
@@ -69,41 +37,5 @@ public class TitleScripting : MonoBehaviour
 		sfxManager.SetMute (PlayerPrefs.GetInt ("SfxMuted") != 0 ? true : false);
 
 		QualitySettings.SetQualityLevel (PlayerPrefs.GetInt ("Quality"));
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (titleState != TitleState.DONE) {
-			Debug.Log ("Time: " + Time.time + " " + (movieStartTime + backgroundMovie.duration));
-			if (Input.GetButtonDown ("Cancel") || Time.time >= (movieStartTime + backgroundMovie.duration)) {
-				// Skip title sequence.
-				presents.color = new Color (presents.color.r, presents.color.g, presents.color.b, 1f);
-				production.color = new Color (production.color.r, production.color.g, production.color.b, 0f);
-				logo.color = new Color (logo.color.r, logo.color.g, logo.color.b, 1f);
-				buttonPanel.SetActive (true);
-				Button[] buttons = buttonPanel.transform.GetComponentsInChildren<Button> ();
-				foreach (Button button in buttons) {
-					ColorBlock colorBlock = button.colors;
-					Color color = colorBlock.normalColor;
-					colorBlock.normalColor = new Color (color.r, color.g, color.b, 1f);
-					button.colors = colorBlock;
-				}
-			
-				Text[] texts = buttonPanel.transform.GetComponentsInChildren<Text> ();
-				foreach (Text text in texts) {
-					Color color = text.color;
-					text.color = new Color (color.r, color.g, color.b, 1f);
-				}
-
-				if (startButton != null) {
-					eventSystem.SetSelectedGameObject (startButton.gameObject);
-					startButton.GetComponent<EventTrigger> ().OnSelect (null);
-				}				
-
-				titleState = TitleState.DONE;
-				backgroundMovie.Stop ();
-			}
-		}
 	}
 }
